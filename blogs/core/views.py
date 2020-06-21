d3bbe5164b03a2b21e7c766ef2205c19fc47da18
arnav0103 <66205884+arnav0103@users.noreply.github.com>
2020-06-21 19:40:52 +0530

    next bar

--- blogs/core/views.py
+++ blogs/core/views.py
@@ -7,10 +7,15 @@ core = Blueprint('core',__name__)
 
 @core.route('/')
 def index():
+    maxi = 0
+    mylist = []
     page = request.args.get('page',1,type = int)
     blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page,per_page=5)
+    for page_num in blog_posts.iter_pages(left_edge=1, right_edge = 1, left_current=1, right_current=2):
+        mylist.append(page_num)
 
-    return render_template('index.htm',blog_posts=blog_posts)
+        maxi = max(mylist)
+    return render_template('index.htm',blog_posts=blog_posts, maxi  =maxi)
 
 @core.route('/info')
 def info():
