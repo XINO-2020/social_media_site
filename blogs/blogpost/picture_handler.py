d861d91590b810f1f1dea6e68662ffe997382d5f
arnav0103 <66205884+arnav0103@users.noreply.github.com>
2020-06-22 15:08:51 +0530

    updates

--- blogs/blogpost/picture_handler.py
+++ blogs/blogpost/picture_handler.py
@@ -12,10 +12,8 @@ def add_blog_pic(pic_upload,username):
     storage_filename = str(username) + '_'+'blog'+ random +'.' +ext_type
     filepath = os.path.join(current_app.root_path, 'static', storage_filename)
 
-    # Play Around with this size.
-    output_size = (200, 200)
+    output_size = (1000,1000)
 
-    # Open the picture and save it
     pic = Image.open(pic_upload)
     pic.thumbnail(output_size)
     pic.save(filepath)
