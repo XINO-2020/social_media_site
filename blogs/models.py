7d57dcf1e46224edfcbb560c92715e7bcd68e83a
arnav0103 <66205884+arnav0103@users.noreply.github.com>
2020-06-24 13:46:47 +0530

    Really good reply update

--- blogs/models.py
+++ blogs/models.py
@@ -41,7 +41,7 @@ class BlogPost(db.Model):
     blog_image = db.Column(db.String(64), nullable = False, default = 'None.png')
     id = db.Column(db.Integer,primary_key=True)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
-    date = db.Column(db.DateTime,nullable = False,default=datetime.utcnow)
+    date = db.Column(db.DateTime,nullable = False,default=datetime.now)
     title = db.Column(db.String(140),nullable=False)
     text = db.Column(db.Text,nullable = False)
 
@@ -56,7 +56,7 @@ class Reply(db.Model):
     __tablename__ = 'reply'
     id = db.Column(db.Integer,primary_key = True)
     reply = db.Column(db.String)
-    time = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    time = db.Column(db.DateTime, nullable = False, default = datetime.now)
     user = db.relationship(User)
     blog = db.relationship(BlogPost)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
