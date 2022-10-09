b3f19e8850518754dd3eb4be4431af3d4596c4ed
Tijil Chhabra <tijilchhabra@gmail.com>
2022-10-09 17:33:37 +0530

    prediction shi

--- blogs/users/forms.py
+++ blogs/users/forms.py
@@ -1,42 +1,75 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField ,SubmitField
-from wtforms.validators import DataRequired, Email , EqualTo, Length
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, RadioField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 from wtforms import ValidationError
-from flask_wtf.file import FileField,FileAllowed
+from flask_wtf.file import FileField, FileAllowed
 
 from flask_login import current_user
 from blogs.models import User
 
+
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(),Email()])
-    password = PasswordField('Password',validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log in')
 
+
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(),Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     fname = StringField('First Name', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators = [DataRequired(),EqualTo('pass_confirm',message = 'Passwords must match'), Length(min = 8, max=16)])
-    pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo(
+        'pass_confirm', message='Passwords must match'), Length(min=8, max=16)])
+    pass_confirm = PasswordField(
+        'Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
-    def validate_email(self,field):
+    def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
-            raise ValidationError('The email you chose has already been registered')
-    def validate_username(self,field):
+            raise ValidationError(
+                'The email you chose has already been registered')
+
+    def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
-            raise ValidationError('The username yuo chose has already been registered')
+            raise ValidationError(
+                'The username yuo chose has already been registered')
+
+
 class UpdateUserForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(),Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired()])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    picture = FileField('Update Profile Picture', validators=[
+                        FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
-    def validate_email(self,field):
+    def validate_email(self, field):
         if field.data != current_user.email:
             if User.query.filter_by(email=field.data).first():
-                raise ValidationError('The email you chose has already been registered')
-    def validate_username(self,field):
+                raise ValidationError(
+                    'The email you chose has already been registered')
+
+    def validate_username(self, field):
         if field.data != current_user.username:
             if User.query.filter_by(username=field.data).first():
-                raise ValidationError('The username you chose has already been registered')
+                raise ValidationError(
+                    'The username you chose has already been registered')
+
+
+class PredictionForm(FlaskForm):
+    age = IntegerField('Age', validators=[DataRequired()])
+    gender = RadioField('Gender', choices=[
+                        (0, 'Male'), (1, 'Female'), (2, 'Trans')])
+    family_history = RadioField('Family History of Mental Illness', choices=[
+                                (1, 'Yes'), (0, 'No')])
+    self_employed = RadioField('Are you Self Employed?', choices=[
+                               (1, 'Yes'), (2, 'No')])
+    remote_work = RadioField('Do you work remotely?',
+                             choices=[(1, 'Yes'), (2, 'No')])
+    tech_company = RadioField('Do you work in a tech company?', choices=[
+                              (1, 'Yes'), (2, 'No')])
+    coworkers = RadioField('Has any of your coworkers suffered from any mental illness?',
+                           choices=[(1, 'Yes'), (0, 'No'), (2, "Don't Know")])
+    wellness_program = RadioField('Have you been a part of any wellness program?', choices=[
+                                  (1, 'Yes'), (0, 'No'), (2, "Don't Know")])
+
+    submit = SubmitField('Submit')
--- blogs/users/views.py
+++ blogs/users/views.py
@@ -1,43 +1,66 @@
- #users/views.py
-from flask import render_template,url_for,flash,redirect,request,Blueprint
+# users/views.py
+from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
-from blogs import  db
-from blogs.models import User,BlogPost
-from blogs.users.forms import RegistrationForm,LoginForm,UpdateUserForm
+from blogs import db
+from blogs.models import User, BlogPost
+from blogs.users.forms import PredictionForm, RegistrationForm, LoginForm, UpdateUserForm
 from blogs.users.picture_handler import add_profile_pic
 from sqlalchemy import desc
 import pickle
 import numpy as np
 
 users = Blueprint('users', __name__)
-#register.login.logout.acoount.bloglist
+# register.login.logout.acoount.bloglist
+
+
 @users.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("core.index"))
 
+
 @users.route('/predict')
 def predict():
-    
+    form = PredictionForm()
+    model = pickle.load(
+        open('C:/Users/tijil/github/social_media_site/blogs/users/model.pkl', 'rb'))
 
-@users.route('/register' , methods = ['GET','POST'])
+    if form.validate_on_submit():
+        int_features = [form.age.data,
+                        form.gender.data, form.family_history.data, form.self_employed.data, form.remote_work.data, form.tech_company.data, form.coworkers.data, form.wellness_program.data]
+        final = [np.array(int_features)]
+        print(final)
+        prediction = model.predict_proba(final)
+
+        output = '{0:.{1}f}'.format(prediction[0][1], 3)
+
+        if output > str(0.5):
+            return render_template('test.htm', pred='You need a treatment.\nProbability of mental illness is {}'.format(output))
+        else:
+            return render_template('test.htm', pred='You do not need treatment.\n Probability of mental illness is {}'.format(output))
+
+    return render_template('test.htm', form=form)
+
+
+@users.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        user = User(fname = form.fname.data, email=form.email.data,
-                    username = form.username.data,
-                    password= form.password.data)
+        user = User(fname=form.fname.data, email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
 
         db.session.add(user)
         db.session.commit()
         flash('Thanks for registration')
         return redirect(url_for('users.login'))
 
-    return render_template('register.htm', form = form)
+    return render_template('register.htm', form=form)
 
-@users.route('/login',methods=['GET','POST'])
+
+@users.route('/login', methods=['GET', 'POST'])
 def login():
     error = ''
     if current_user.is_authenticated == False:
@@ -46,24 +69,25 @@ def login():
 
             user = User.query.filter_by(email=form.email.data).first()
 
-            if user is not None and user.check_password(form.password.data) :
+            if user is not None and user.check_password(form.password.data):
 
                 login_user(user)
                 flash('Log in Success!')
 
                 next = request.args.get('next')
-                if next == None or not next[0] =='/':
+                if next == None or not next[0] == '/':
                     next = url_for('core.index')
                 return redirect(next)
             elif user is not None and user.check_password(form.password.data) == False:
                 error = 'Wrong Password'
             elif user is None:
                 error = 'No such login Pls create one'
-        return render_template('login.htm', form=form, error = error)
+        return render_template('login.htm', form=form, error=error)
     else:
         return render_template('login_logout.htm')
 
-@users.route('/account',methods = ['GET','POST'])
+
+@users.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
     pic = current_user.profile_image
@@ -72,10 +96,9 @@ def account():
         current_user.email = form.email.data
         current_user.username = form.username.data
 
-
         if form.picture.data is not None:
             id = current_user.id
-            pic = add_profile_pic(form.picture.data,id)
+            pic = add_profile_pic(form.picture.data, id)
             current_user.profile_image = pic
 
         flash('User Account Created')
@@ -86,11 +109,9 @@ def account():
         form.username.data = current_user.username
         form.email.data = current_user.email
 
+    profile_image = url_for('static', filename=current_user.profile_image)
+    return render_template('account.htm', profile_image=profile_image, form=form, pic=pic)
 
-
-
-    profile_image = url_for('static', filename= current_user.profile_image)
-    return render_template('account.htm', profile_image=profile_image, form=form, pic = pic)
 
 @users.route("/<username>")
 def user_posts(username):
@@ -98,10 +119,11 @@ def user_posts(username):
     mylist = []
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
-    blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page,per_page = 5)
+    blog_posts = BlogPost.query.filter_by(author=user).order_by(
+        BlogPost.date.desc()).paginate(page=page, per_page=5)
 
-    for page_num in blog_posts.iter_pages(left_edge=1, right_edge = 1, left_current=1, right_current=2):
+    for page_num in blog_posts.iter_pages(left_edge=1, right_edge=1, left_current=1, right_current=2):
         mylist.append(page_num)
 
     maxi = max(mylist)
-    return render_template('user_blog_post.htm', blog_posts = blog_posts, user = user,maxi = maxi)
+    return render_template('user_blog_post.htm', blog_posts=blog_posts, user=user, maxi=maxi)
